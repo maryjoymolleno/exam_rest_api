0ed2f3c4c64d138486b026818645ed3f17b57558
Mary Joy <116539958+maryjoymolleno@users.noreply.github.com>
2023-01-11 18:05:04 +0800

    Add files via upload

--- retrieve.js
+++ retrieve.js
@@ -1,1 +1,16 @@
+const DB = require('../database/Connection')
 
+module.exports = async (fields) => {
+  try {
+    const query = SELECT  +
+                    ${fields}  +
+                  FROM  +
+                    accounts
+
+    const results = await DB(query)
+
+    return results
+  } catch (error) {
+    return []
+  }
+}
