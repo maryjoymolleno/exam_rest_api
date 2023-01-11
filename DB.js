f6458bf7acb8c474a827c44c8624dd9088e769f6
Mary Joy <116539958+maryjoymolleno@users.noreply.github.com>
2023-01-11 18:02:08 +0800

    Add files via upload

--- DB.js
+++ DB.js
@@ -1,1 +1,33 @@
+const mysql = require('mysql')
 
+const dbConfig = {
+  host: "localhost",
+  user: "root",
+  password: "",
+  port: 3306,
+  database: "nodejs-mysql"
+}
+
+const db = mysql.createPool(dbConfig)
+
+module.exports = (query) => {
+  return new Promise((resolve, reject) => {
+    db.getConnection((error, sql) => {
+      if (error) {
+        console.log("Database error: ", error)
+        reject(error)
+      } else {
+        sql.query(query, (error, results) => {
+          if (error) {
+            console.log("Query error: ", error)
+            reject(error)
+          } else {
+            resolve(results)
+          }
+
+          sql.release()
+        })
+      }
+    })
+  })
+}
