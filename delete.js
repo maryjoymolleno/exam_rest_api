ddbb645a472169fcaedfbd88cd95e3c7df6897b3
Mary Joy <116539958+maryjoymolleno@users.noreply.github.com>
2023-01-11 18:06:42 +0800

    Add files via upload

--- delete.js
+++ delete.js
@@ -1,1 +1,16 @@
+const DB = require('../database/Conection')
 
+module.exports = async (id) => {
+  try {
+    const query = DELETE FROM  +
+                    accounts  +
+                  WHERE  +
+                    id = ${id}
+
+    await DB(query)
+
+    return true
+  } catch (error) {
+    return false
+  }
+}
