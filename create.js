46cb195e48ecf2bb658d72424e92140d783acab5
Mary Joy <116539958+maryjoymolleno@users.noreply.github.com>
2023-01-11 18:04:00 +0800

    Add files via upload

--- create.js
+++ create.js
@@ -1,1 +1,16 @@
+const DB = require('../database/Connection')
 
+module.exports = async (username, password) => {
+  try {
+    const query = INSERT INTO +
+                    accounts  +
+                  VALUES  +
+                    (null, '${username}', '${password}')
+
+    await DB(query)
+
+    return true
+  } catch (error) {
+    return false
+  }
+}
